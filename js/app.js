7aa74acfd38582960d27ba8caab430c6e69d7354
Owen <owenyangg@gmail.com>
2016-03-16 20:37:20 -1000

    removed DevLeague default and changed to local storage

--- js/app.js
+++ js/app.js
@@ -8,7 +8,41 @@ document.getElementById('abc').addEventListener("keyup", function(event){
     kids.setStudents(event.target.value);
     event.target.value = "";
   }
+  localStorage.setItem('kids',kids.getStudents());
 });
+
+document.getElementById('LS').onclick = function(){
+
+  students = localStorage.getItem('kids').split(",");
+
+  classA = meanRevertingBeuller(students);
+
+  var start = document.getElementById("start");
+
+  for (var i in students){
+    var p = document.createElement('p');
+    var row = start.appendChild(p);
+    row.id = students[i];
+    document.getElementById(students[i]).innerHTML = students[i];
+  }
+
+  switch(document.getElementById('MR').value){
+    case "full":
+      degreeOfMeanReversion = 0;
+      break;
+    case "partial":
+      degreeOfMeanReversion = 5;
+      break;
+    case "random":
+      degreeOfMeanReversion = 10;
+      break;
+
+  }
+};
+
+document.getElementById('CS').onclick = function(){
+  localStorage.removeItem('kids');
+};
 
 document.getElementById("commence").onclick = function(){
   students = kids.getStudents();
@@ -28,7 +62,7 @@ document.getElementById("commence").onclick = function(){
       degreeOfMeanReversion = 0;
       break;
     case "partial":
-      degreeOfMeanReversion = 2;
+      degreeOfMeanReversion = 5;
       break;
     case "random":
       degreeOfMeanReversion = 10;
@@ -53,31 +87,3 @@ document.getElementById("run").onclick = function(){
     picked + " " + times;
 };
 
-
-document.getElementById("DL").onclick = function(){
-  students = ["Stephen", "Tyler", "Owen", "Taylor", "Malia", "Laura", "Nat", "Pam"];
-  classA = meanRevertingBeuller(students);
-
-  var start = document.getElementById("start");
-
-  for (var i in students){
-    var p = document.createElement('p');
-    var row = start.appendChild(p);
-    row.id = students[i];
-    document.getElementById(students[i]).innerHTML = students[i];
-  }
-
-
-  switch(document.getElementById('MR').value){
-    case "full":
-      degreeOfMeanReversion = 0;
-      break;
-    case "partial":
-      degreeOfMeanReversion = 2;
-      break;
-    case "random":
-      degreeOfMeanReversion = 10;
-      break;
-
-  }
-};
